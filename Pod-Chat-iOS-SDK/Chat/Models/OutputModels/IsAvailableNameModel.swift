6421d9c445ed89ad9d192447054b672c201e4ff1
Mahyar <mahyarjananeh289@gmail.com>
2020-03-15 15:01:30 +0330

    implement isNameAvailable method

---
+++ Pod-Chat-iOS-SDK/Chat/Models/OutputModels/IsAvailableNameModel.swift
@@ -0,0 +1,9 @@
+//
+//  IsAvailableNameModel.swift
+//  FanapPodChatSDK
+//
+//  Created by MahyarZhiani on 12/25/1398 AP.
+//  Copyright © 1398 Mahyar Zhiani. All rights reserved.
+//
+
+import Foundation
