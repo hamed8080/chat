e3f7f504da9430cec151d57975abe4cd85deaee7
Hamed Hoseeini <hamed8080@gmail.com>
2023-12-17 14:49:54 +0330

    - Removed unused added event

--- Sources/Chat/Events/ParticipantEventTypes.swift
+++ Sources/Chat/Events/ParticipantEventTypes.swift
@@ -12,7 +12,6 @@ import Foundation
 public enum ParticipantEventTypes {
     case participants(ChatResponse<[Participant]>)
     case add(ChatResponse<Conversation>)
-    case added(ChatResponse<[Participant]>)
     case deleted(ChatResponse<[Participant]>)
     case setAdminRoleToUser(ChatResponse<[AdminRoleResponse]>)
     case removeAdminRoleFromUser(ChatResponse<[AdminRoleResponse]>)
