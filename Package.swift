bc7068e0594d6cdb726d6924d4a5deaea0e9e6fe
Hamed Hoseeini <hamed8080@gmail.com>
2024-05-20 12:50:04 +0330

    Package: Use local path of SDKs in development environment

--- Package.swift
+++ Package.swift
@@ -14,20 +14,20 @@ let package = Package(
         .library(name: "Chat", targets: ["Chat"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/chat-extensions", from: "2.0.0"),
+        .package(path: "../ChatExtensions"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "Chat",
             dependencies: [
-                .product(name: "ChatExtensions", package: "chat-extensions"),
+                .product(name: "ChatExtensions", package: "ChatExtensions"),
             ],
             resources: []
         ),
         .testTarget(name: "ChatTests", dependencies: [
             "Chat",
-            .product(name: "ChatExtensions", package: "chat-extensions"),
+            .product(name: "ChatExtensions", package: "ChatExtensions"),
         ], path: "Tests"),
     ]
 )
