2ebfd4122e2f42c97c6e6ff3d7d2eac2a9c05407
Hamed Hoseeini <hamed8080@gmail.com>
2024-05-27 14:46:48 +0330

    Versioning: Bump up the version to 2.0.1
    Change the git base URL

--- Package.swift
+++ Package.swift
@@ -14,20 +14,20 @@ let package = Package(
         .library(name: "Chat", targets: ["Chat"]),
     ],
     dependencies: [
-        .package(path: "../ChatExtensions"),
+        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/chat-extensions", from: "2.0.1"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "Chat",
             dependencies: [
-                .product(name: "ChatExtensions", package: "ChatExtensions"),
+                .product(name: "ChatExtensions", package: "chat-extensions"),
             ],
             resources: []
         ),
         .testTarget(name: "ChatTests", dependencies: [
             "Chat",
-            .product(name: "ChatExtensions", package: "ChatExtensions"),
+            .product(name: "ChatExtensions", package: "chat-extensions"),
         ], path: "Tests"),
     ]
 )
