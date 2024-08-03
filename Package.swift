1a3cf7ba270f23679a892f3c5151fc00221af8e4
Hamed Hoseeini <hamed8080@gmail.com>
2024-08-03 15:41:41 +0330

    Use a variable to detecet local and remote packages

--- Package.swift
+++ Package.swift
@@ -2,6 +2,18 @@
 
 import PackageDescription
 
+let useLocalDependency = true
+
+let local: [Package.Dependency] = [
+    .package(path: "../ChatExtensions"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+]
+
+let remote: [Package.Dependency] = [
+    .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-extensions", from: "2.1.2"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+]
+
 let package = Package(
     name: "Chat",
     defaultLocalization: "en",
@@ -13,24 +25,18 @@ let package = Package(
     products: [
         .library(name: "Chat", targets: ["Chat"]),
     ],
-    dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-extensions", from: "2.1.2"),
-//           .package(path: "../ChatExtensions"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    ],
+    dependencies: useLocalDependency ? local : remote,
     targets: [
         .target(
             name: "Chat",
             dependencies: [
-                .product(name: "ChatExtensions", package: "chat-extensions"),
-//                .product(name: "ChatExtensions", package: "ChatExtensions"),
+                .product(name: "ChatExtensions", package: useLocalDependency ? "ChatExtensions" : "chat-extensions"),
             ],
             resources: []
         ),
         .testTarget(name: "ChatTests", dependencies: [
             "Chat",
-            .product(name: "ChatExtensions", package: "chat-extensions"),
-//            .product(name: "ChatExtensions", package: "ChatExtensions"),
+            .product(name: "ChatExtensions", package: useLocalDependency ? "ChatExtensions" : "chat-extensions"),
         ], path: "Tests"),
     ]
 )
