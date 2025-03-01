49c9b529904f0975ce4528bbfbc468591c4cc732
Hamed Hoseeini <hamed8080@gmail.com>
2025-03-01 20:08:40 +0300

    [Reafcator] Remove fa_ir [Refactor] Move base 64 decoder / encoders to additive

--- submodules/Additive/Sources/Additive/Date+.swift
+++ submodules/Additive/Sources/Additive/Date+.swift
@@ -41,7 +41,7 @@ public extension Date {
     /// MMM: Name of the month = November
     @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *) func timeAgoSinceDateCondense(local: Locale = .current) -> String? {
         Date.formatter.locale = local
-        let isPersian = local.identifier == "fa_IR"
+        let isPersian = local.identifier == "ZmFfSVI=".fromBase64()
         let cal = isPersian ? Date.pCal : Calendar.current
 
         if cal.isDateInToday(self) {
@@ -62,7 +62,7 @@ public extension Date {
     private static let dtf = DateFormatter()
     @available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *) func timeOrDate(local: Locale = .current) -> String? {
         Date.dtf.locale = local
-        let isPersian = local.identifier == "fa_IR"
+        let isPersian = local.identifier == "ZmFfSVI=".fromBase64()
         var cal = isPersian ? Date.pCal : Calendar.current
         cal.locale = local
         if cal.isDateInToday(self) {
@@ -80,7 +80,7 @@ public extension Date {
     // It will return "15 October 2024"
     func dayMonthNameYear(local: Locale = .current) -> String? {
         Date.dtf.locale = local
-        let isPersian = local.identifier == "fa_IR"
+        let isPersian = local.identifier == "ZmFfSVI=".fromBase64()
         var cal = isPersian ? Date.pCal : Calendar.current
         cal.locale = local
 
